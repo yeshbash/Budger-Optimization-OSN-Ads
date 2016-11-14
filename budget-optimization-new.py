e557c44e1ae32eabc41124464a7021ac52743e70
yeshbash <yeshbash@gmail.com>
2016-11-14 00:00:21 -0800

    New Algo
    - Cost data corrections
    - New Algorithm corrections
    - User creation

---
+++ budget-optimization-new.py
@@ -0,0 +1,149 @@
+import pickle
+import collections
+from decimal import *
+
+getcontext().prec = 3
+################Utility Methods########################
+
+#Returns label set in dictionary key format
+def get_lable_key(label_list):
+	return tuple(sorted(label_list))
+
+#Loads serialized data
+def load_data(path):
+	with open(path,"rb") as f:
+		return pickle.load(f)
+#Returns users matching labels in label_set
+def get_users(label_set):
+	l_users = {}
+	for i,l in enumerate(label_set):
+		if i ==0:
+			l_users = set(label_users[l])
+		else:
+			l_users = l_users&label_users[l]
+	return l_users
+
+def initialize_data():
+	label_cost = load_data("C:\\Users\\yeshwanth\\Test\\label_costs")
+	label_users = load_data("C:\\Users\\yeshwanth\\Test\\label_users")
+	label_attribues = load_data("C:\\Users\\yeshwanth\\Test\\attribute_values")
+	return label_cost,label_users,label_attribues
+
+################Algorithm Methods########################	
+
+#Extends current label set by adding an optimal attribute. Attribute with highest cummulative RI is chosen optimal
+#Param : cur_ls - Current Label Set | label_ri - Current Ratio of Increment List | u_st - Target Users
+#Return : labels_found - Status of extension | label_ri - Updated RI List
+def extend_labelset(cur_ls, label_ri,u_st):
+	#Selecting unused label attributes
+	unused_l = label_attribues.keys() - [i.split(":")[0] for i in cur_ls ]
+	labels_found = False
+	if len(unused_l) >0:
+		attribute_ri = collections.defaultdict(Decimal)
+		attr_label_ri = collections.defaultdict(list)
+		for attr in unused_l:
+			for val in label_attribues[attr]: #Domain of each unused attribute
+				ext_l = attr+":"+val
+				ext_lk = get_lable_key(list(cur_ls)+[ext_l])
+				u_ext_lk = get_users(ext_lk)
+				common_users = u_ext_lk & u_st
+				if len(common_users) > 0 and label_cost[ext_lk] < label_cost[cur_ls]:
+					#Calculating the ratio of increment
+					ri_val = getcontext().divide(len(common_users)* label_cost[cur_ls],label_cost[ext_lk]*len(u_ext_lk))
+					attr_label_ri[attr].append((ext_lk,ri_val))
+					attribute_ri[attr] += ri_val
+		#Deducing the optimal attribute to add
+		if len(attribute_ri) >0 :
+			labels_found = True
+			opt_attr = max(attribute_ri.keys(), key = lambda k : attribute_ri[k])
+			print("Optimal Attribute : ",opt_attr )
+			for l_ri in attr_label_ri[opt_attr]:
+				label_ri[l_ri[0]] = l_ri[1] #Including labels of optimal attribute to RI list
+			print("Total Lables Added on top of",cur_ls,":", len(attr_label_ri[opt_attr]))
+	
+	return labels_found,label_ri
+
+# Allocates 'budget' to the label set 'ls'
+# Param : ls - target label set | budget - budget allocation for ls | users - total uncovered users
+# Return: remaining budget, remaining uncovered users from input 'users'
+
+def allocate_budget(ls, budget, users):
+	global users_covered
+	ls_users = get_users(ls)
+	#Allocation = Minimum of covering entire set or the available budget
+	allocation = min(label_cost[ls]*len(ls_users), budget)
+	label_budget[ls] = allocation #Adding to budget distribution
+	b_budget = budget - allocation
+	if allocation==label_cost[ls]*len(ls_users):
+		b_users  = users - ls_users #Remaining uncovered users 
+	else:
+		b_users = users
+	users_covered += int(allocation/label_cost[ls]) #Users covered stats tracker
+	print("Budger Allocation:\nLabel:",ls,"\nBudger Allocated:",allocation,"\nUsers Covered",  allocation/label_cost[ls],"\nRemaining Budger:",b_budget)
+	
+	return b_budget,b_users
+
+#Optimized Budget Distribution for a given Budget and Preferred Label set
+#Param : label_set - Preferred Label Set | b_0 - Advertiser Budger | u_st - Target Users
+def optimize_budget(label_set, b_0, u_st):
+	label_ri = collections.defaultdict(Decimal)
+	#Initial Extension of Label set
+	extension, label_ri = extend_labelset(label_set,label_ri,u_st)
+	if extension == True:
+		while b_0 >0 and len(u_st) >0: #Iterates untill budget available or untill all users are covered
+			if len(label_ri) >0: #If optimal lables available for adding
+				max_ri_label = max(label_ri.keys(),key = lambda k : label_ri[k])
+				print("Max RI lable:",max_ri_label)
+				del label_ri[max_ri_label]
+				if label_cost[max_ri_label]*len(get_users(max_ri_label)) <= b_0:
+					#Budget available to target entire optimal label set
+					print("Cost Sufficient")
+					b_0,u_st = allocate_budget(max_ri_label,b_0,u_st)
+				else:
+					#Budget insufficient to target entire optimal label set. Initates further extension
+					extension, label_ri = extend_labelset(max_ri_label,label_ri,u_st)
+					print("Further Extension Done")
+					if extension == True:
+						continue
+					else:
+						print("No more extension possible")
+						b_0,u_st = allocate_budget(max_ri_label,b_0,u_st)
+			else:
+				#Allocates maximum budget to current optimal label set
+				b_0,u_st = allocate_budget(label_set,b_0,u_st)
+	else:
+		#No optimization possible.
+		b_0,u_st = allocate_budget(label_set,b_0,u_st)
+
+		
+
+################Main Logic########################
+		
+print("[Debug] Initializing Data")
+#Loading label_cost and label_users from data set file
+label_cost,label_users,label_attribues = initialize_data()
+
+print("[Debug] Initization Complete")
+
+print("Enter a key to continue. 'N' to exit")
+while(input()!="N"):
+	#Initialization
+	users_covered = 0
+	label_budget = collections.defaultdict(Decimal)
+
+	l_p = input("Preferred Label:")
+	l_p_key = get_lable_key([i.strip() for i in l_p.split(",")])
+	u_st = get_users(l_p_key) #Target User Set
+	print("Total users in input label set: ",len(u_st))
+	print("Cost per user: ",label_cost[l_p_key])
+	
+	b_0 = Decimal(input("Budget:"))
+	
+	print("[Debug] Algorithm Begin")
+	
+	optimize_budget(l_p_key,b_0,u_st)
+	
+	print("Result Summary\n",label_budget)
+	print("Users Covered:", users_covered)
+	print("Extra users covered:", users_covered - int(b_0/label_cost[l_p_key]))
+	print("Enter a key to continue. 'N' to exit")
--- data-creation/data_creation.py
+++ data-creation/data_creation.py
@@ -10,98 +10,110 @@ gender_attr = "Gender"
 eth_attr = "Ethinicity"
 income_attr = "Income"
 
-def generate_price(start,end):
-	return Decimal(random.uniform(start,end))/Decimal(1)
+def generate_price(type):
+	if type ==1:
+		return Decimal(random.uniform(6,12))/Decimal(1)
+	elif type ==2:
+		return Decimal(random.uniform(5,10))/Decimal(1)
+	elif type ==3:
+		return Decimal(random.uniform(4,8))/Decimal(1)
+	elif type ==4:
+		return Decimal(random.uniform(4,6))/Decimal(1)
+	elif type ==5:
+		return Decimal(random.uniform(3,5))/Decimal(1)
+	elif type ==6:
+		return Decimal(random.uniform(2,4))/Decimal(1)
 
 label_sets = {}
 with open("C:\\Users\\yeshwanth\\Test\\attribute_values","rb") as f:
 	attribute_values = pickle.load(f)
 
 for loc in attribute_values["Location"]:
-	label_sets[tuple(sorted({loc_attr+":"+loc}))]= generate_price(1,12)
+	label_sets[tuple(sorted({loc_attr+":"+loc}))]= generate_price(1)
 	for gender in attribute_values["Gender"]:
-		label_sets[tuple(sorted({gender_attr+":"+gender}))]= generate_price(1,12)
-		label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender}))]= generate_price(1,12)
+		label_sets[tuple(sorted({gender_attr+":"+gender}))]= generate_price(1)
+		label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender}))]= generate_price(2)
 		for edu in attribute_values["Education"]:
-			label_sets[tuple(sorted({edu_attr+":"+edu}))]= generate_price(1,12)
-			label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu}))]= generate_price(1,12)
-			label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu}))]= generate_price(1,12)
+			label_sets[tuple(sorted({edu_attr+":"+edu}))]= generate_price(1)
+			label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu}))]= generate_price(2)
+			label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu}))]= generate_price(2)
+			label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu}))]= generate_price(3)
 			for age in attribute_values["Age"]:
 				#4C1
-				label_sets[tuple(sorted({age_attr+":"+age}))]= generate_price(1,12)
+				label_sets[tuple(sorted({age_attr+":"+age}))]= generate_price(1)
 				#4C2
-				label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age}))]= generate_price(1,12)
-				label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age}))]= generate_price(1,12)
-				label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(1,12)
+				label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age}))]= generate_price(2)
+				label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age}))]= generate_price(2)
+				label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(2)
 				#4C3
-				label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age}))]= generate_price(1,12)
-				label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(1,12)
-				label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(1,12)
+				label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age}))]= generate_price(3)
+				label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(3)
+				label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(3)
 				#4C4
-				label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(1,12)
+				label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age}))]= generate_price(4)
 				for income in attribute_values["Income"]:
 					#5C1
-					label_sets[tuple(sorted({income_attr+":"+income}))]= generate_price(1,12)
+					label_sets[tuple(sorted({income_attr+":"+income}))]= generate_price(1)
 					#5C2
-					label_sets[tuple(sorted({loc_attr+":"+loc,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({gender_attr+":"+gender,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({age_attr+":"+age,income_attr+":"+income}))]= generate_price(1,12)
+					label_sets[tuple(sorted({loc_attr+":"+loc,income_attr+":"+income}))]= generate_price(2)
+					label_sets[tuple(sorted({gender_attr+":"+gender,income_attr+":"+income}))]= generate_price(2)
+					label_sets[tuple(sorted({edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(2)
+					label_sets[tuple(sorted({age_attr+":"+age,income_attr+":"+income}))]= generate_price(2)
 					#5C3
-					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income}))]=generate_price(1,12)
+					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,income_attr+":"+income}))]= generate_price(3)
+					label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(3)
+					label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age,income_attr+":"+income}))]= generate_price(3)
+					label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(3)
+					label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income}))]= generate_price(3)
+					label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income}))]=generate_price(3)
 					#5C4
-					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(1,12)
-					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(1,12)
+					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(4)
+					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income}))]= generate_price(4)
+					label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(4)
+					label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age,edu_attr+":"+edu,income_attr+":"+income}))]= generate_price(4)
 					#5C5				
-					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income}))]= generate_price(1,12)
+					label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income}))]= generate_price(5)
 					for eth in attribute_values["Ethinicity"]:
 						#6C1
-						label_sets[tuple(sorted({eth_attr+":"+eth}))]= generate_price(1,12)
+						label_sets[tuple(sorted({eth_attr+":"+eth}))]= generate_price(1)
 						#6C2
-						label_sets[tuple(sorted({loc_attr+":"+loc,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
+						label_sets[tuple(sorted({loc_attr+":"+loc,eth_attr+":"+eth}))]= generate_price(2)
+						label_sets[tuple(sorted({gender_attr+":"+gender,eth_attr+":"+eth}))]= generate_price(2)
+						label_sets[tuple(sorted({edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(2)
+						label_sets[tuple(sorted({age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(2)
+						label_sets[tuple(sorted({income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(2)
 						#6C3
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]=generate_price(1,12)
-						label_sets[tuple(sorted({edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({loc_attr+":"+loc,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({gender_attr+":"+gender,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]=generate_price(3)
+						label_sets[tuple(sorted({edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(3)
+						label_sets[tuple(sorted({age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(3)
 						#6C4
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(1,12)						
-						label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)												
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(4)
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(4)
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(4)
+						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(4)
+						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(4)						
+						label_sets[tuple(sorted({loc_attr+":"+loc,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(4)												
 						#label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
+						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(4)
+						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(4)
+						label_sets[tuple(sorted({gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(4)
+						label_sets[tuple(sorted({edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(4)
 						#label_sets[tuple(sorted({age_attr+":"+age,edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
 						#6C5
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,eth_attr+":"+eth}))]= generate_price(5)
+						label_sets[tuple(sorted({gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(5)
+						label_sets[tuple(sorted({loc_attr+":"+loc,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(5)
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(5)
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(5)
 						#6C6
-						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(1,12)
+						label_sets[tuple(sorted({loc_attr+":"+loc,gender_attr+":"+gender,edu_attr+":"+edu,age_attr+":"+age,income_attr+":"+income,eth_attr+":"+eth}))]= generate_price(6)
 
 with open("C:\\Users\\yeshwanth\\Test\\label_costs","wb") as f:
 	pickle.dump(label_sets,f)
